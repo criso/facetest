f70950b96d61287627336befcf72f9d7baeeb535
Cristiano Oliveira <ocean.cris@gmail.com>
2011-11-17 20:18:08 -0500

    using promises

--- tests/users.test.js
+++ tests/users.test.js
@@ -31,87 +31,12 @@ vows.describe("testUser.test").addBatch({
         .end(function(self) {
            
         });
-
-// --------------------------------------
-// api
-
-facetest
-  // overrides options
-  .options({ installed: true })
-  // sets that the user 'Magic Man'
-  // is friends with 'an array of users'
-  // This should automatically create those users
-  // and send friend requests
-  .friends({
-    'Magic Man': ['El Diablo', 'Ricky Bobby']  
-  });
-
-
-facetest
-  .user('Magic Man')
-  .friendsWith(['El Diablo', 'Ricky Booby']);
-
-
-facetest.on('end', function () { })
-
-
-
-
-
-
-
-
-
-      // .getUsers();
-
-     return users;
     },
+    
+    "": "pending"
 
-    'chaining': function () {
-      console.log(arguments);
-
-    },
-
-    'user should be created': 'pending'
-
-  
   }
 
-
-
-// }).addBatch({
-//   "With test users": {
-//     topic: function () {
-//       fbTest.createUser("Magic Man", this.callback);
-//     },
-// 
-//     "we should be able to create *user 1*": function(res) {
-//       assert.isNotNull(res);
-//     },
-// 
-//     "after creating *user 1*": {
-//       topic: function (res) {
-//         fbTest.createUser("El diablo", this.callback);
-//       },
-// 
-//       "we should be able to create *user 2*": function(res) {
-//         assert.isNotNull(res);
-//       },
-// 
-//       "and *user2* ": {
-//         topic: function (res) {
-//           fbTest.friendRequest({
-//               from:  "Magic Man"
-//             , to:    "El Diablo"
-//           }, this.callback);
-//         },
-// 
-//         "*user2* should accept friend request": function (res) {
-//           assert.equal(res.data, "true");
-//         }
-//       }
-//     }
-//   }
 }).addBatch({
 
   "When tests are over": {
